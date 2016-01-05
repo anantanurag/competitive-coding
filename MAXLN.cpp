ded93b7837e3dd62439e0f0c06a14dffd725bd2b
arttp2 <mail.asquared@gmail.com>
2016-01-05 23:25:29 +0530

    WA

---
+++ MAXLN.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	long long int n, r;
+	double s = 0.00;
+
+	cin >> n;
+	for(int i = 0; i < n; i++)
+	{
+		cin >> r;
+		s = (4*r*r) + 0.25;
+		cout << "Case " << i + 1 << ": " << setprecision(3) << s << endl;
+		s = 0.00;
+	}
+}
